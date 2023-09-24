80c0df3a575fd9e277c1784f6063e0780cd237e6
benny <benny08118@gmail.com>
2023-09-24 21:16:46 +0800

    update paginator

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
+class PublishedManager(models.Manager):
+    def get_queryset(self) -> QuerySet:
+       return super(PublishedManager,self).get_queryset()\
+       .filter(status='published')
 class Post(models.Model):
   STATUS_CHOICE=(
     ('draft','Draft'),
@@ -13,11 +19,19 @@ unique_for_date='publish')
   author=models.ForeignKey(User,
   on_delete=models.CASCADE,
   related_name='blog_posts')
-  body   =models.TextField()
-  publish=models.DateTimeField(default=timezone.now)
-  created=models.DateTimeField(auto_now=True)
-  update =models.DateTimeField(auto_now=True)
-  state  =models.CharField(max_length=10,choices=STATUS_CHOICE,default='draft')
+  body    =models.TextField()
+  publish =models.DateTimeField(default=timezone.now)
+  created =models.DateTimeField(auto_now=True)
+  update  =models.DateTimeField(auto_now=True)
+  status  =models.CharField(max_length=10,choices=STATUS_CHOICE,default='draft')
+  objects =models.Manager() 
+  published =PublishedManager()
+
+  def get_absolute_url(self):
+     return reverse('blog:post_detail',args=[self.publish.year,
+                                             self.publish.month,
+                                             self.publish.day,
+                                             self.slug])
   class Meta:
     ordering=('-publish',)
 
